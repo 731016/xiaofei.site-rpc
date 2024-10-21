39b0cb4c5b7e6e9ed90e5ea305584e370e99dfce
tuaofei <tu_aofei@163.com>
2024-10-21 21:59:29 +0800

    更新mock模拟接口

--- rpc-common/src/main/java/site/xiaofei/common/service/UserService.java
+++ rpc-common/src/main/java/site/xiaofei/common/service/UserService.java
@@ -16,11 +16,28 @@ public interface UserService {
      */
     User getUser(User user);
 
-    /**
-     * 获取数字
-     * @return
-     */
-    default short getNumber(){
+    //=====================以下为mock模拟接口 开发测试使用 START=====================
+    default short getMockNumber(){
         return 1;
     }
+
+    default boolean getMockBoolean(){
+        return true;
+    }
+
+    default int getMockInt(){
+        return 1;
+    }
+
+    default long getMockLong(){
+        return 1L;
+    }
+
+    default User getMockUser(){
+        User user = new User();
+        user.setName("测试");
+        return user;
+    }
+
+    //=====================以下为mock模拟接口 开发测试使用 END=====================
 }
